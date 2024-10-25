07d4b617481e62bd250cb3bb0d80b5a9b3211a2a
beva3 <mizabienvenu@gmail.com>
2024-10-25 17:54:28 +0530

    make a turtle

--- snake_game1.py
+++ snake_game1.py
@@ -7,5 +7,16 @@ wn.bgcolor("green")
 wn.setup(width=600, height=600)
 wn.tracer(0) #desactiver la mise a jour automatique de l'ecran
 
+#Snake head
+head = tr.Turtle()
+head.speed(0)
+head.shape("square")
+head.color("black")
+head.goto(0,0)
+head.direction = "stop"
+
+# Main game loop
+while True:
+    wn.update()
 
 wn.mainloop()
