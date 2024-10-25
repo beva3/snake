a478af33ee1043de3d70661ea007a7f3ff1e55ed
beva3 <mizabienvenu@gmail.com>
2024-10-25 18:42:44 +0530

    you should learn turtle

--- snake_game1.py
+++ snake_game1.py
@@ -11,12 +11,26 @@ wn.setup(width=600, height=600)
 wn.tracer(0) #desactiver la mise a jour automatique de l'ecran
 
 #Snake head
+def go_up():
+    head.direction == "up"
+
+def go_down():
+    head.direction == "down"
+
+def go_right():
+    head.direction == "right"
+
+def go_left():
+    head.direction == "left"
+
+
+
 head = tr.Turtle()
 head.speed(0)
 head.shape("square")
 head.color("black")
 head.goto(0,0)
-head.direction = "right"
+head.direction = "stop"
 
 # function
 def move():
@@ -35,6 +49,13 @@ def move():
     if head.direction == "left":
         x = head.xcor()
         head.sety(x - 20)
+
+#keyboard bindings
+wn.listen()
+wn.onkeypress(go_up,"w")
+wn.onkeypress(go_down,"s")
+wn.onkeypress(go_right,"d")
+wn.onkeypress(go_left,"a")
 
 # Main game loop
 while True:
