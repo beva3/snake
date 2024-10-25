7c9382cd5dab8ca0c8dfea4f180593b5cda624f0
beva3 <mizabienvenu@gmail.com>
2024-10-25 17:45:35 +0530

    creat screen

---
+++ snake_game1.py
@@ -0,0 +1,11 @@
+import turtle as tr # tortu
+
+#setup the screen
+wn = tr.Screen()
+wn.title("Snake game")
+wn.bgcolor("green")
+wn.setup(width=600, height=600)
+wn.tracer(0) #desactiver la mise a jour automatique de l'ecran
+
+
+wn.mainloop()
