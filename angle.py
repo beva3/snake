5a9450eb0f2077b1babc0938aa6aca6167e31b03
beva3 <mizabienvenu@gmail.com>
2024-10-26 09:17:42 +0530

    creat and move

---
+++ angle.py
@@ -0,0 +1,16 @@
+import turtle as tr
+
+# move the pen in the forward direction by 100px
+tr.forward(110)
+
+# changes the durection od the pen by 10deg in the left direction
+tr.left(110)
+
+# move the pen in the forward direction in the new direction by 110px
+tr.forward(110)
+
+# changes the durection od the pen by 10deg in the left direction
+tr.left(110)
+
+# move the pen in the forward direction in the new direction by 110px
+tr.forward(110)
