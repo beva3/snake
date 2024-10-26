316af7f505eefc1123dfa175d84684a7fae60556
beva3 <mizabienvenu@gmail.com>
2024-10-26 09:45:03 +0530

    desiner un carre

---
+++ carre.py
@@ -0,0 +1,17 @@
+import turtle as tr
+import time as t
+delay = 2
+
+# desiner un carree
+c = 200 
+for i in range(4):
+    print(f"forward {c}") 
+    tr.forward(c)
+
+    t.sleep(delay)
+    print(f"left 90")
+    tr.left(90)
+
+    t.sleep(delay)
+
+tr.exitonclick()
