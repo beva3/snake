5cd5396fbb05dd2d3f5a331fa8c436c404b38682
beva3 <mizabienvenu@gmail.com>
2024-10-26 10:41:35 +0530

    the end

--- circle.py
+++ circle.py
@@ -1,8 +1,17 @@
 import turtle as tr
 
 # methode to draw patern of circle with rad radius
+def draw(rad):
+    # draw circle
+    tr.circle(rad)
 
-tr.circle(100)
+    tr.up()
+    tr.setpos(0,-rad)
+    tr.down()
+
+# loop for patern
+for i in range(5):
+    draw(20 + 20*i)
 
 # exit on click
 tr.exitonclick()
