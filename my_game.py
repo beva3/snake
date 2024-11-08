957e4ab0638bf51e6b0149f1f03c02e74b530da8
beva3 <mizabienvenu@gmail.com>
2024-11-08 18:12:07 +0530

    creat class segment, nouriture

--- my_game.py
+++ my_game.py
@@ -13,6 +13,28 @@ class JeuSnake:
         self.win.listen()   # Active lecoute des events
 
 
+class Segment(_tr.Turtle):
+    def __init__(self,x,y):
+        super().__init__()
+        self.speed(0)   # pas d'animatoin
+        self.shape("square")    # Forme de chaque segment
+        self.color("green")     # Couleur des segments su segment
+        self.penup()            # Pas de trace des segments
+        self.goto(x,y)          # Placer le segment a une positoin donnee
+
+class Nouriture(_tr.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.speed(0)           # pas d'animation
+        self.shape("circle")    # Forme de la nouriture
+        self.color("red")       # Couleur de la nouriture
+        self.penup()            # Pas de trace de la nouriture
+        self.deplacer()         # Déplacer la nouriture a une nouvelle position aléatoire
+
+    def deplacer(self):
+        # placer la nouriture a une position aleatoir dans la fenetre
+        self.goto(_rnd.randint(-290, 290), _rnd.randint(-290, 290))
+
 def main():
     jeu = JeuSnake()
 
