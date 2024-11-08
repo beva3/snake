ba705b830b5a1bf0b492d050beb1e615755f18ba
beva3 <mizabienvenu@gmail.com>
2024-11-08 18:58:03 +0530

    game working

--- my_game.py
+++ my_game.py
@@ -5,13 +5,50 @@ import time as _t
 #Configuration de la Fenêtre de Jeu
 class JeuSnake:
     def __init__(self):
-        self.win = _tr.Screen() # Crée une fenêtre pour le jeu
-        self.win.title("Jeu Snake") # Définit le titre de la fenêtre
-        self.win.bgcolor("black")   #  Couleur de fond du fenetre
-        self.win.setup(width=600, height=600)   # size fenetre
-        self.win.tracer(0)  # Desactive le tracer pour le rendu instantané
-        self.win.listen()   # Active lecoute des events
+        self.window = _tr.Screen()
+        self.window.title("Jeu Snake")
+        self.window.bgcolor("black")
+        self.window.setup(width=600, height=600)
+        self.window.tracer(0)
+        self.serpent = Serpent()
+        self.nourriture = Nouriture()
+        self.window.listen()
+        # Associe les touches du clavier au mouvement
+        self.window.onkey(self.monter, "Up")
+        self.window.onkey(self.descendre, "Down")
+        self.window.onkey(self.aller_gauche, "Left")
+        self.window.onkey(self.aller_droite, "Right")
 
+    def jouer(self):
+        while not self.serpent.game_over:
+            self.window.update()
+            self.serpent.deplacer_serpent()
+            if self.serpent.collision_murs():
+                self.serpent.afficher_game_over(self.window)
+                break
+            if self.serpent.serpent[0].distance(self.nourriture) < 20:
+                self.nourriture.deplacer()
+                self.serpent.ajouter_segment()
+            _t.sleep(0.1)
+
+        self.window.update()
+        self.window.mainloop()
+
+    def monter(self):
+        if self.serpent.serpent[0].heading() != 270:
+            self.serpent.serpent[0].setheading(90)
+
+    def descendre(self):
+        if self.serpent.serpent[0].heading() != 90:
+            self.serpent.serpent[0].setheading(270)
+
+    def aller_gauche(self):
+        if self.serpent.serpent[0].heading() != 0:
+            self.serpent.serpent[0].setheading(180)
+
+    def aller_droite(self):
+        if self.serpent.serpent[0].heading() != 180:
+            self.serpent.serpent[0].setheading(0)
 
 class Segment(_tr.Turtle):
     def __init__(self,x,y):
@@ -35,8 +72,55 @@ class Nouriture(_tr.Turtle):
         # placer la nouriture a une position aleatoir dans la fenetre
         self.goto(_rnd.randint(-290, 290), _rnd.randint(-290, 290))
 
+class Serpent:
+    def __init__(self):
+        self.serpent = []           # Liste de seg du serpent
+        self.taille_serpent = 1     # taille du serpent
+        self.game_over = 0          # Game over
+        self.creer_serpent()
+    def creer_serpent(self):
+        # Ajout un segment de depart
+        for i in range(self.taille_serpent):
+            segment = Segment(-20 * i, 0)
+            self.serpent.append(segment)
+    
+    def deplacer_serpent(self):
+        # Deplacent de segment du corps
+        if self.game_over:
+            return  # Arreter le mvt si game over
+        for i in range(len(self.serpent) -1 , 0, -1):
+            x = self.serpent[i -1].xcor()
+            y = self.serpent[i -1].ycor()
+            self.serpent[i].goto(x, y)
+        self.serpent[0].forward(20)
+    
+    def ajouter_segment(self):
+        # Ajout un segment a la fin du serprnt
+        segment = Segment(self.serpent[-1].xcor(), self.serpent[-1].ycor())
+        self.serpent.append(segment)
+    
+    def collision_murs(self):
+        # Vérifie si le serpent touche un mur
+        X = self.serpent[0].xcor()
+        Y = self.serpent[0].ycor()
+        if X > 290 or X < -290 or Y > 290 or Y < -290:
+            self.game_over = True  # Game Over
+            return True
+        return False
+    
+    def afficher_game_over(self, window):
+        # Affiche le message Game Over à l'écran
+        texte = _tr.Turtle()
+        texte.hideturtle()
+        texte.color("white")
+        texte.penup()
+        texte.goto(0, 0)
+        texte.write("Game Over!", align="center", font=("Courier", 24, "normal"))
+        window.update()
+
 def main():
-    jeu = JeuSnake()
+    jeu = JeuSnake()  # Crée le jeu
+    jeu.jouer()  # Lance la boucle de jeu
 
 if __name__ == "__main__":
     main()
